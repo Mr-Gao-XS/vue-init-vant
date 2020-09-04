d1103e9c90417f882a14bc68a8cf86fcb626e9d4
gaozhong <53365381@qq.com>
2020-09-04 14:20:38 +0800

    新增密码正则

--- src/utils/regexp.js
+++ src/utils/regexp.js
@@ -3,6 +3,11 @@ const regexp = {
   phoneReg(phone) {
     return !/^1[3456789]\d{9}$/.test(phone)
   },
+
+  // 密码 6-20位只能包含 字母 或者字母+数字 必须字母开头
+  passwordReg(password) {
+    return /^[a-zA-Z]\w{5,19}$/.test(password)
+  },
 }
 
 export default regexp
